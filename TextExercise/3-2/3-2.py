7701859607c6291c5d142286193d884c857e4f86
Koji Nagahara <k-nagahara@rough-and-ready.co.jp>
2018-07-25 23:29:06 +0900

    予約語に引っかかってそうな変数名を修正

--- TextExercise/3-2/3-2.py
+++ TextExercise/3-2/3-2.py
@@ -1,8 +1,8 @@
 origin = '1.4, 19.2, 7.8'
 
 textList = origin.split(',')
-sum = 0.0
+total = 0.0
 for i in range(0, len(textList)) :
-    sum += float(textList[i])
+    total += float(textList[i])
 
-print('合計は', sum)
+print('合計は', total)
