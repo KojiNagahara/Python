ba5d29b8e147464f982485e0fc3ce677a65db4fe
Koji Nagahara <kjnghr@gmail.com>
2019-03-18 16:38:24 +0900

    コメント追加

--- TextExercise/13-2/test.py
+++ TextExercise/13-2/test.py
@@ -1,8 +1,10 @@
+"""動的計画法の実装をテストするモジュール。"""
 import random
 from item import Item
 from rootedBinaryTree import max_value
 
 def small_test():
+  """小規模テスト"""
   names = ['a', 'b', 'c', 'd']
   values = [6, 7, 8, 9]
   weights = [3, 3, 2, 5]
@@ -15,12 +17,14 @@ def small_test():
   print(f'取得したItemの総価値：{value}')
 
 def build_many_items(number_of_items, max_value, max_weight):
+  """big_test用のデータ作成自動化"""
   items = []
   for i in range(number_of_items):
     items.append(Item(str(i), random.randint(1, max_value), random.randint(1, max_value)))
   return items
 
 def big_test(number_of_items):
+  """大規模テスト。ただあまり大規模になると視認性が悪くなるので少し表示に工夫する必要はある"""
   items = build_many_items(number_of_items, 10, 10)
   print('対象Item：')
   for item in items:
