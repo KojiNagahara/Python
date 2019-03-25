a30b722ac14991b31e3e878282ecbbe41c1fe43d
Koji Nagahara <kjnghr@gmail.com>
2019-03-25 18:25:51 +0900

    推計統計学のためのコイン投げのシミュレーション追加

---
+++ TextExercise/15-1/flip.py
@@ -0,0 +1,26 @@
+"""コイン投げのシミュレーションによる大数の法則の確認"""
+import random
+
+def flip(number):
+  """numberは正の整数"""
+  heads = 0
+  for i in range(number):
+    if random.choice(('H', 'T')) == 'H':
+      heads += 1
+  return heads/number
+
+def simulate(number_for_trial, trial_number):
+  """number_for_trial、trial_numberともに正の整数"""
+  frac_heads = []
+  for i in range(trial_number):
+    frac_heads.append(flip(number_for_trial))
+  mean = sum(frac_heads)/len(frac_heads)
+  return mean
+
+if __name__ == '__main__':
+  for i in range(10):
+    print(f'コイン投げ10回の試行の平均：{simulate(10, 1)}')
+  for i in range(10):
+    print(f'コイン投げ10回の試行を100回実行した場合の平均：{simulate(10, 100)}')
+  print(f'コイン投げ100回の試行を10000回実行した場合の平均：{simulate(100, 10000)}')
+  print(f'コイン投げ100回の試行を10000回実行した場合の平均：{simulate(100, 10000)}')
